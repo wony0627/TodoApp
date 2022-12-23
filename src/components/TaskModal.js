40cfca6765501ef49e3f6af1de80976abf766239
wony0627 <119390816+wony0627@users.noreply.github.com>
2022-12-23 22:55:09 +0900

    Create TaskModal.js

---
+++ src/components/TaskModal.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import {
+    View,
+    TextInput,
+    StyleSheet,
+} from 'react-native'
+import Modal from 'react-native-modal'
+
+const TaskModal = ({
+    isVisible,
+    add,
+    hide,
+}) => {
+    let content = ''
+    return (
+        <Modal
+        isVisible={isVisible}
+        onBackdropPress={hide}
+        avoidKeyboard
+        style={styles.modal}
+        >
+            <View style={styles.container}>
+                <TextInput
+                onChangeText={(text) => {
+                    content = text
+                }}
+                onEndEditing={() => add(content)}
+                placeholder="새로운 할 일을 추가해 주세요"
+                />
+            </View>
+        </Modal>
+    )
+}
+
+const styles = StyleSheet.create({
+    modal: {
+        justifyContent: 'flex-end',
+        margin: 0,
+    },
+    container: {
+        padding: 16,
+        backgroundColor: '#FFFFFF',
+        borderTopLeftRadius: 8,
+        borderTopRightRadius: 8,
+    },
+})
+
+export default TaskModal
